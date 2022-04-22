cfdaf9e8d75b9bc8e8aa689a785ccf66af4e91ca
almoyen <itsmoyen@gmail.com>
2022-04-22 20:48:49 +0300

    Added Save Profile Component

---
+++ pages/save-profile.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SaveProfile from '../components/ProfilePage/SaveProfile/SaveProfile'
+
+export default function saveprofile() {
+  return (
+   <SaveProfile/>
+  )
+}
